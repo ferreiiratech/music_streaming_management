4e45766f2b65119a0b42d174adf22005a0b93abd
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-10 16:57:08 -0300

    first commit

---
+++ src/main/java/org/leonardo/music_streaming_management/MusicStreamingManagementApplication.java
@@ -0,0 +1,13 @@
+package org.leonardo.music_streaming_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStreamingManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicStreamingManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/leonardo/music_streaming_management/MusicStreamingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.leonardo.music_streaming_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStreamingManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
