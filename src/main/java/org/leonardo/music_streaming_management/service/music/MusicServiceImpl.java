c9db837ebcacb01f4414af53aa138280b2a12959
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-12 01:48:04 -0300

    MusicRepository dependency injection into MusicServiceImpl

--- src/main/java/org/leonardo/music_streaming_management/service/music/MusicServiceImpl.java
+++ src/main/java/org/leonardo/music_streaming_management/service/music/MusicServiceImpl.java
@@ -1,7 +1,14 @@
 package org.leonardo.music_streaming_management.service.music;
 
+import org.leonardo.music_streaming_management.repository.music.MusicRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MusicServiceImpl implements IMusicService {
+    private final MusicRepository musicRepository;
+
+    public MusicServiceImpl(MusicRepository musicRepository) {
+        this.musicRepository = musicRepository;
+    }
+
 }
