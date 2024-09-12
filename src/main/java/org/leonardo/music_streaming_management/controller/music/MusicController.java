c40da374463918ea47efb58564034b5aad09776b
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-12 02:01:11 -0300

    creating the http endpoint to create a song

--- src/main/java/org/leonardo/music_streaming_management/controller/music/MusicController.java
+++ src/main/java/org/leonardo/music_streaming_management/controller/music/MusicController.java
@@ -1,13 +1,24 @@
 package org.leonardo.music_streaming_management.controller.music;
 
+import org.leonardo.music_streaming_management.dto.music.MusicCreateRequestDTO;
+import org.leonardo.music_streaming_management.dto.music.MusicCreatedResponseDTO;
 import org.leonardo.music_streaming_management.service.music.IMusicService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api/music")
 public class MusicController {
     private final IMusicService musicService;
 
     public MusicController(IMusicService musicService) {
         this.musicService = musicService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<MusicCreatedResponseDTO> createMusic(@RequestBody MusicCreateRequestDTO musicCreateRequestDTO) {
+        MusicCreatedResponseDTO musicCreatedResponseDTO = musicService.createMusic(musicCreateRequestDTO);
+        return ResponseEntity.ok(musicCreatedResponseDTO);
     }
 }
