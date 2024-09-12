e90c8457a3c32ac90c1b0ea7cfb244b5a804d9e3
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-11 22:06:40 -0300

    implementation of the IMusicService dependency in the MusicController class

--- src/main/java/org/leonardo/music_streaming_management/controller/music/MusicController.java
+++ src/main/java/org/leonardo/music_streaming_management/controller/music/MusicController.java
@@ -1,7 +1,13 @@
 package org.leonardo.music_streaming_management.controller.music;
 
+import org.leonardo.music_streaming_management.service.music.IMusicService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MusicController {
+    private final IMusicService musicService;
+
+    public MusicController(IMusicService musicService) {
+        this.musicService = musicService;
+    }
 }
