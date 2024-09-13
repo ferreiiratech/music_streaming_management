4d9e4b513995dcfa545964902348bee4f1a9ef86
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-13 19:13:33 -0300

    implements the method of create music in service interface

--- src/main/java/org/leonardo/music_streaming_management/service/music/IMusicService.java
+++ src/main/java/org/leonardo/music_streaming_management/service/music/IMusicService.java
@@ -1,4 +1,8 @@
 package org.leonardo.music_streaming_management.service.music;
 
+import org.leonardo.music_streaming_management.dto.music.MusicCreateRequestDTO;
+import org.leonardo.music_streaming_management.dto.music.MusicCreatedResponseDTO;
+
 public interface IMusicService {
+    MusicCreatedResponseDTO createMusic(MusicCreateRequestDTO musicCreateRequestDTO);
 }
