70e1f89364c345941738568710206b19682ebfcf
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-13 19:06:17 -0300

    creation of the InvalidMusicArtistException class

---
+++ src/main/java/org/leonardo/music_streaming_management/model/music/exception/InvalidMusicArtistException.java
@@ -0,0 +1,7 @@
+package org.leonardo.music_streaming_management.model.music.exception;
+
+public class InvalidMusicArtistException extends RuntimeException {
+    public InvalidMusicArtistException(String message) {
+        super(message);
+    }
+}
