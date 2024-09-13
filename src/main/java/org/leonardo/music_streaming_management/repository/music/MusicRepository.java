376ae48e617b00aa6d98cdf0b3fb5e2c63be29a2
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-13 19:13:08 -0300

    implements the method of searching by music title

--- src/main/java/org/leonardo/music_streaming_management/repository/music/MusicRepository.java
+++ src/main/java/org/leonardo/music_streaming_management/repository/music/MusicRepository.java
@@ -4,6 +4,9 @@ import org.leonardo.music_streaming_management.model.music.MusicEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MusicRepository extends JpaRepository<MusicEntity, Long> {
+    Optional<MusicEntity> findByTitle(String title);
 }
