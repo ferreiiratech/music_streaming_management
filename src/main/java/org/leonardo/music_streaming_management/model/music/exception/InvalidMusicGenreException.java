676549ce63402eb1cf9b7be1b77d0dd52c07de46
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-13 19:07:25 -0300

    creation of the InvalidMusicGenreException class

---
+++ src/main/java/org/leonardo/music_streaming_management/model/music/exception/InvalidMusicGenreException.java
@@ -0,0 +1,7 @@
+package org.leonardo.music_streaming_management.model.music.exception;
+
+public class InvalidMusicGenreException extends RuntimeException {
+    public InvalidMusicGenreException(String message) {
+        super(message);
+    }
+}
