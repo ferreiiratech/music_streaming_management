e8e1e9a18b30fbce95ea41663f089f3d5ffc8f4f
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-13 19:14:09 -0300

    implements the method of create music in controller

--- src/main/java/org/leonardo/music_streaming_management/controller/music/MusicController.java
+++ src/main/java/org/leonardo/music_streaming_management/controller/music/MusicController.java
@@ -4,11 +4,10 @@ import org.leonardo.music_streaming_management.dto.music.MusicCreateRequestDTO;
 import org.leonardo.music_streaming_management.dto.music.MusicCreatedResponseDTO;
 import org.leonardo.music_streaming_management.service.music.IMusicService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/music")
+@RestController
+@RequestMapping("/api/music")
 public class MusicController {
     private final IMusicService musicService;
 
