a19b81b92dce09710ee5a076e03f054540239772
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-13 19:12:23 -0300

    implements the method of searching by album title

--- src/main/java/org/leonardo/music_streaming_management/repository/album/AlbumRepository.java
+++ src/main/java/org/leonardo/music_streaming_management/repository/album/AlbumRepository.java
@@ -4,6 +4,9 @@ import org.leonardo.music_streaming_management.model.album.AlbumEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AlbumRepository extends JpaRepository<AlbumEntity, Long> {
+    Optional<AlbumEntity> findByTitle(String album);
 }
