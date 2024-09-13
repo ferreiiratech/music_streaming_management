179ab6c38ab97e8d9560cb49a006c2e8b810894e
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-13 19:12:44 -0300

    implements the method of searching by artist name

--- src/main/java/org/leonardo/music_streaming_management/repository/artist/ArtistRepository.java
+++ src/main/java/org/leonardo/music_streaming_management/repository/artist/ArtistRepository.java
@@ -4,6 +4,9 @@ import org.leonardo.music_streaming_management.model.artist.ArtistEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ArtistRepository extends JpaRepository<ArtistEntity, Long> {
+    Optional<ArtistEntity> findByName(String artist);
 }
