9f4678614417488d34959542b321ccbeebb60b6d
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-13 19:05:59 -0300

    creation of the InvalidMusicAlbumException class

---
+++ src/main/java/org/leonardo/music_streaming_management/model/music/exception/InvalidMusicAlbumException.java
@@ -0,0 +1,7 @@
+package org.leonardo.music_streaming_management.model.music.exception;
+
+public class InvalidMusicAlbumException extends RuntimeException {
+    public InvalidMusicAlbumException(String message) {
+        super(message);
+    }
+}
