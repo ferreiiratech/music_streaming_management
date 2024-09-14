6fcd9f2ba948657de2329fbcd3514bb059d77749
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-14 20:40:20 -0300

    creation InvalidArtistNationalityException class

---
+++ src/main/java/org/leonardo/music_streaming_management/model/artist/exception/InvalidArtistNationalityException.java
@@ -0,0 +1,7 @@
+package org.leonardo.music_streaming_management.model.artist.exception;
+
+public class InvalidArtistNationalityException extends RuntimeException {
+    public InvalidArtistNationalityException(String message) {
+        super(message);
+    }
+}
