3213d7ba4b8c6e7d1fb75139083012381e0a7e18
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-14 03:05:17 -0300

    modifying config of path server

--- src/main/java/org/leonardo/music_streaming_management/controller/music/MusicController.java
+++ src/main/java/org/leonardo/music_streaming_management/controller/music/MusicController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/music")
+@RequestMapping("/music")
 public class MusicController {
     private final IMusicService musicService;
 
