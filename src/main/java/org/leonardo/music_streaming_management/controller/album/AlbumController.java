f5aafd67380366a97788763dee18862d415f444e
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-14 17:00:26 -0300

    injection of AlbumRepository into AlbumController

--- src/main/java/org/leonardo/music_streaming_management/controller/album/AlbumController.java
+++ src/main/java/org/leonardo/music_streaming_management/controller/album/AlbumController.java
@@ -1,7 +1,15 @@
 package org.leonardo.music_streaming_management.controller.album;
 
+import org.leonardo.music_streaming_management.repository.album.IAlbumRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/album")
 public class AlbumController {
+    private final IAlbumRepository albumRepository;
+
+    public AlbumController(IAlbumRepository albumRepository) {
+        this.albumRepository = albumRepository;
+    }
 }
