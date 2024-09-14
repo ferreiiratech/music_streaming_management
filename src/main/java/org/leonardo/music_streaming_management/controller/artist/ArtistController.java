3c0a09716c35e364f3c2080f08b0aa9a3cad996e
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-14 16:11:17 -0300

    creation of the method of creating an artist in ArtistController

--- src/main/java/org/leonardo/music_streaming_management/controller/artist/ArtistController.java
+++ src/main/java/org/leonardo/music_streaming_management/controller/artist/ArtistController.java
@@ -1,7 +1,23 @@
 package org.leonardo.music_streaming_management.controller.artist;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.leonardo.music_streaming_management.dto.artist.*;
+import org.leonardo.music_streaming_management.service.artist.IArtistService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/artist")
 public class ArtistController {
+    private final IArtistService artistService;
+
+    public ArtistController(IArtistService artistService) {
+        this.artistService = artistService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ArtistCreatedResponseDTO> createArtist(@RequestBody ArtistRequestDTO artistRequestDTO) {
+        ArtistCreatedResponseDTO artistCreatedResponseDTO = artistService.createArtist(artistRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(artistCreatedResponseDTO);
+    }
 }
