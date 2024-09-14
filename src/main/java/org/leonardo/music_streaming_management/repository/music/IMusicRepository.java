615641c27d1f35dfb1f8fe4bba5f57cb29f4d9c6
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-14 11:47:13 -0300

    creates the method that searches paged songs

--- src/main/java/org/leonardo/music_streaming_management/repository/music/IMusicRepository.java
+++ src/main/java/org/leonardo/music_streaming_management/repository/music/IMusicRepository.java
@@ -1,12 +1,15 @@
 package org.leonardo.music_streaming_management.repository.music;
 
 import org.leonardo.music_streaming_management.model.music.MusicEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MusicRepository extends JpaRepository<MusicEntity, Long> {
+public interface IMusicRepository extends JpaRepository<MusicEntity, Long> {
     Optional<MusicEntity> findByTitle(String title);
+    Page<MusicEntity> findAll(Pageable pageable);
 }
