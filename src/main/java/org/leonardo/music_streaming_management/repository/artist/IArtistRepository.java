bed6c12da00d12fcb74b690b89443c7e0ebaa220
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-14 16:06:58 -0300

    creation method findAll pageable in repository

--- src/main/java/org/leonardo/music_streaming_management/repository/artist/IArtistRepository.java
+++ src/main/java/org/leonardo/music_streaming_management/repository/artist/IArtistRepository.java
@@ -1,12 +1,15 @@
 package org.leonardo.music_streaming_management.repository.artist;
 
 import org.leonardo.music_streaming_management.model.artist.ArtistEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ArtistRepository extends JpaRepository<ArtistEntity, Long> {
+public interface IArtistRepository extends JpaRepository<ArtistEntity, Long> {
     Optional<ArtistEntity> findByName(String artist);
+    Page<ArtistEntity> findAll(Pageable pageable);
 }
